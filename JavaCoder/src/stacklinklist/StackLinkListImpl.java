718ad5edceaed947f3e9bc95c74bd841c4e6e4b2
algokelvin-373 <kelvinht373@gmail.com>
2022-03-06 21:43:33 +0700

    [Java] [Stack with LinkList] Done

---
+++ JavaCoder/src/stacklinklist/StackLinkListImpl.java
@@ -0,0 +1,10 @@
+package stacklinklist;
+
+interface StackLinkListImpl {
+    boolean isEmpty();
+    void push(int data);
+    void pop();
+    void makeEmpty();
+    void print();
+    void top();
+}
